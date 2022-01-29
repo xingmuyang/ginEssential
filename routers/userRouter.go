2fd9bfff2005adfad8faed6211d9358a567bec10
彭兆尊 <pengzhaozun@bingosoft.net>
2022-01-29 17:44:51 +0800

    路由分离

---
+++ routers/userRouter.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"learn/ginEssential/controllers"
+)
+
+
+func LoadRouter(r *gin.Engine) {
+	r.POST("/api/auth/register", controllers.Register)
+}
